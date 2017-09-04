77701bf44d635f7fb6dfa5a9848d451bb5df2ea7
Sushant Kumar Gadi <SushantKumar.Gadi@gartner.com>
2017-09-04 19:17:48 +0530

    latest code

--- src/main/java/com/opensource/kagazi/rest/CoreController.java
+++ src/main/java/com/opensource/kagazi/rest/CoreController.java
@@ -18,7 +18,7 @@ import io.swagger.annotations.ApiOperation;
 
 @RestController
 @RequestMapping(value = "kagazi/core")
-@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
+@CrossOrigin(origins = {"http://localhost:4200","https://kagazi.000webhostapp.com"}, maxAge = 3600)
 public class CoreController {
 
 	static final Logger LOGGER = Logger.getLogger(CoreController.class);
